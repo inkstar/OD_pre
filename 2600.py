f82cd0e194d1531b62ba55a78fb66e6a535c8fd3
inkstar <1247207199@qq.com>
2024-02-12 17:33:52 +0800

    1

--- 2600.py
+++ 2600.py
@@ -1,20 +1,18 @@
 
 dic={')':'(',']':'[','}':'{'}
 stack=[]
-s=list(input())
-flag=1
-depth=0
+s=list(input())             #输入字符串的同时存储于列表内部
+
+depth=0                     #记录栈的深度
 for i in s:
     if i in dic and stack:
         if stack[-1]==dic[i]:
             depth=max(depth,len(stack))
             stack.pop()
-        else :
-            flag=False
     else:
         stack.append(i)
-if not stack:
+if not stack:               #栈为空，说明全部匹配，则输出深度
     print(depth)
-else:
+else:                       #栈不为空，说明部分匹配，输出0,表示不能完全匹配
     print('0')
 
